13ba0266d731465c9c45c2b4519877f250395c20
Galiao Goncalves <galiaogoncalves@kieback-peter.de>
2026-05-15 22:44:24 +0200

    added events into Board

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { renderTodoItem } from './component/TodoItem/todoItem.ts'
 import { TodoService } from './services/TodoService.ts'
+import type { BoardTypes } from './types/todoTypes.ts'
 
 const form = document.getElementById('todo-submit-form') as HTMLFormElement
 const currentTodo = document.getElementById('todo-input') as HTMLInputElement
@@ -8,11 +9,13 @@ const boardList = document.querySelectorAll(
   '.open_todos_container, .progress_todos_container, .blocked_todos_container, .done_todos_container',
 )
 
-// constante
+// import TodoService and store it
 const service = new TodoService()
 
 // submit new TodoItem
-form?.addEventListener('submit', (e) => {
+form?.addEventListener('submit', submitTodoItem)
+
+function submitTodoItem(e: Event): void {
   e.preventDefault()
 
   const todoValue = currentTodo.value.trim()
@@ -22,9 +25,10 @@ form?.addEventListener('submit', (e) => {
 
   service.addTodo(todoValue)
   renderTodoList()
+  bindTodoItemsEvents()
   currentTodo.value = ''
   currentTodo.focus()
-})
+}
 
 // render TodoList here
 function renderTodoList(): void {
@@ -37,22 +41,36 @@ function renderTodoList(): void {
 }
 
 boardList.forEach((list) => {
-  list.addEventListener('dragover', (e) => {
+  list.addEventListener('dragover', (e: Event): void => {
     e.preventDefault()
-    console.log('TESTT Drag', e)
+    console.log('TESTT Drag', e.currentTarget)
   })
 
   list.addEventListener('drop', (e) => {
     e.preventDefault()
-    console.log('TEST DROP', e)
     const targetBoard = getBoardFromList(list as Element)
     console.log('targetBoard', targetBoard)
   })
 })
 
-function getBoardFromList(list: Element): 'open' | 'in_progress' | 'blocked' | 'done' {
-  if (list.classList.contains('open_todos')) return 'open'
-  if (list.classList.contains('progress_todos')) return 'in_progress'
-  if (list.classList.contains('blocked_todos')) return 'blocked'
+addEventListener('drag', (event) => {
+  console.log('Hello', event.currentTarget)
+})
+
+function getBoardFromList(list: Element): BoardTypes {
+  if (list.classList.contains('open_todos_container')) return 'open'
+  if (list.classList.contains('progress_todos_container')) return 'in_progress'
+  if (list.classList.contains('blocked_todos_container')) return 'blocked'
   return 'done'
+}
+
+// todoItemElement events
+function bindTodoItemsEvents(): void {
+  const todoItemElements = document.querySelectorAll<HTMLElement>('.todo-item')
+
+  todoItemElements.forEach((todoItem) => {
+    todoItem.addEventListener('dragstart', (e: DragEvent) => {
+      console.log('ROM TODO ITEM', e.currentTarget)
+    })
+  })
 }
