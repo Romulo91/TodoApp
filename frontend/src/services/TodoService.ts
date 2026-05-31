a4c490697c4c8f6b3b9f51aa6d47b58368f4ef34
Galiao Goncalves <galiaogoncalves@kieback-peter.de>
2026-05-31 22:41:56 +0200

    added clg info

--- frontend/src/services/TodoService.ts
+++ frontend/src/services/TodoService.ts
@@ -5,6 +5,7 @@ export class TodoService {
   private todos: TodoItem[] = []
 
   addTodo(text: string, board: BoardTypes): TodoItem {
+    console.info('added an ToDo item')
     const todo = new Todo(crypto.randomUUID(), text, board)
     this.todos.push(todo)
     return todo
@@ -19,10 +20,12 @@ export class TodoService {
     if (list === null) return
     const todo = this.todos.find((item) => item.id === list.dataset['id'])
     if (!todo) return console.error(`something went wrong:  ${todo}`)
+    console.info(`moved todo to ${targetBoard}`)
     todo.board = targetBoard
   }
 
   removeTodo(todoId: string): void {
+    console.info('remove Todo item')
     this.todos = this.todos.filter((li) => li.id !== todoId)
   }
 }
